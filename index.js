84fea1c886849ad114f7c145d0eb90ded6655bd8
Christian Garcia <bones192nd@gmail.com>
2023-01-12 13:40:33 -0500

    Lab, didn't finish yet

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+// saturdayFun test
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+// mondayWork test
+const mondayWork = function (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork(activity)
+
+// wrapAdjective test
